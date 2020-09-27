78671367d1a73cb670a0fa471465c2ed03a22327
sairam <sairam.pandy@digitalsoft.com>
2020-09-27 11:04:38 +0530

    Add project files.

---
+++ DatabaseConnection/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace DatabaseConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string connectionString= $@"Data Source = DESKTOP-FKSO33P\SQLEXPRESS; Initial Catalog = DigitalsoftFactory; Persist Security Info = True; user id = sa; password = p10father_25587; MultipleActiveResultSets = True";
+
+            //Creating a sql connection object 
+            SqlConnection cnn = new SqlConnection(connectionString);
+            //open the connection 
+            cnn.Open();
+            // Get Connection details 
+            string connectionDetails = GetConnectionProperties(cnn);
+            //close the connection 
+            cnn.Close();
+            //dispose the connection 
+            cnn.Dispose();
+            // print the connection details 
+            Console.WriteLine(connectionDetails);
+            Console.ReadLine();
+        }
+
+        public static string GetConnectionProperties(SqlConnection cnn)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Connection String : " + cnn.ConnectionString);
+            sb.AppendLine("State : " + cnn.State.ToString());
+            sb.AppendLine("Connection TimeOut : " + cnn.ConnectionTimeout);
+            sb.AppendLine("DataBase : " + cnn.Database);
+            sb.AppendLine("DataSource : " + cnn.DataSource);
+            sb.AppendLine("Server Version : " + cnn.ServerVersion);
+            sb.AppendLine("WorkStation Id : " + cnn.WorkstationId);
+            return sb.ToString();
+        }
+    }
+}
